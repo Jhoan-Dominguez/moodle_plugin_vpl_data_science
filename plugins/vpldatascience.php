6fa5eac407fa24ce32a6e2973ede31eddebd2f80
jhoan.dominguez <jhoandominguezu@gmail.com>
2024-09-17 10:38:11 -0500

    add: add example page

---
+++ plugins/vpldatascience.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Muestra el contenido de la página
+ *
+ * @package   local_vpldatascience
+ * @copyright 2024 Universidad Distrital - Facultad Tecnologica: Jhoan Dominguez, Jeisson Sanchez
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+global $DB, $OUTPUT, $PAGE;
+
+// Verifique todas las variables requeridas.
+$courseid = required_param('id', PARAM_INT);
+require_login($courseid);
+
+// Definimos nuestra página
+$PAGE->set_url('/local/vpldatascience/vpldatascience.php', array('id' => $courseid));
+$PAGE->set_pagelayout('standard');
+
+$PAGE -> requires -> js('/local/vpldatascience/js/main.js');
+
+echo $OUTPUT->header();
+echo html_writer::tag('h2',get_string('page_title', 'local_vpldatascience'));
+echo html_writer::div( `Mensaje: {{message}}`, '', array('id' => 'app') );
+
+echo $OUTPUT->footer();
