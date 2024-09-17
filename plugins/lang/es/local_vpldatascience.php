edba34bbc375253f6e0e273156a3d3af3b97693e
jhoan.dominguez <jhoandominguezu@gmail.com>
2024-09-17 10:36:10 -0500

    add: add lang folder, and some vars to use with function get_string

---
+++ plugins/lang/es/local_vpldatascience.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Cadenas de idioma 'es_mx'
+ *
+ * @package    local_vpldatascience
+ * @copyright  2024 Universidad Distrital - Facultad Tecnologica: Jhoan Dominguez, Jeisson Sanchez
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Configuracion
+$string["plugin_name"] = "VPL Data Science";
+$string["setting_vpldatascience"] = "VPL Data Science";
+$string["enable_vpldatascience"] = "Activar VPL Data Science";
+$string["enable_vpldatascience_desc"] = "Selecciona la casilla para activar el VPL Data Science";
+$string["auth_token_vpldatascience"] = "Token de Autenticacion para el VPL Data Science";
+$string["auth_token_vpldatascience_desc"] = "Ingrese el Token de autenticacion para poder accede al VPL Data Science";
+$string["defauth_token_vpldatascience_desc"] = "Null";
+
+
+// Barra de Navegacion
+$string["barnav_option_title"] = "VPL Data Science";
+
+// Elementos de la pagina VPL Data Science
+$string["page_title"] = "VPL Data Science";
+$string["op1"] = "Hola";
+
