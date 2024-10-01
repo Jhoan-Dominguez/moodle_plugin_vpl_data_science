e6c7bfc7abb8e5c2421117d840e45928777c0368
jhoan.dominguez <jhoandominguezu@gmail.com>
2024-10-01 04:35:18 -0500

    update: minimun updates

--- execute.sh
+++ execute.sh
@@ -1,6 +1,10 @@
 
-rm -r ./moodle/local/vpldatascience/
-rm -r ./vpldatascience/assets/
+# rm -r ./moodle/local/vpldatascience/
+# rm -r ./vpldatascience/assets/
 
-docker cp b108ae893970390505e63e20b75dce2d57281fa033c2b04945f350478dfaabeb:/home/vue-projects/vpl-data-science/dist/assets/ ./vpldatascience/assets/
-cp -r ./vpldatascience/ ./moodle/local/
+# docker cp b108ae893970390505e63e20b75dce2d57281fa033c2b04945f350478dfaabeb:/home/vue-projects/vpl-data-science/dist/assets/ ./vpldatascience/assets/
+# cp -r ./vpldatascience/ ./moodle/local/
+
+# ssh -i "vpl-datascience.pem" ubuntu@ec2-44-204-79-30.compute-1.amazonaws.com
+
+# /home/ubuntu/moodle_vpl/moodle/local/vpldatascience/
